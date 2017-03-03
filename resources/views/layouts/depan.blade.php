81939835341b2c17431f8aece6eb53100322ad3e
tarikhagustia <agustia.tarikh150@gmail.com>
2017-03-02 19:11:13 -0500

    Change Templates

--- resources/views/layouts/depan.blade.php
+++ resources/views/layouts/depan.blade.php
@@ -13,7 +13,9 @@
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">Guest Book</a>
+  <a class="navbar-brand" href="#">
+    <img src="/images/logo.png" />
+  </a>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
@@ -22,6 +24,38 @@
       <li class="nav-item">
         <a class="nav-link" href="{{ route('checkout') }}">Checkout</a>
       </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Report
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="/report/checkin">Guest Report</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          User Management
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="/user/create">Create New User</a>
+        </div>
+      </li>
+    </ul>
+    <ul class="navbar-nav navbar-right">
+        <!-- Authentication Links -->
+        @if (Sentinel::guest())
+            <li><a href="">Login</a></li>
+            <li><a href="">Register</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{ Sentinel::getUser()->email }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="/logout">Logout</a>
+          </div>
+        </li>
+        @endif
     </ul>
   </div>
 </nav>
